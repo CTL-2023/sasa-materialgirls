bd373bc9930cdc5cbf2f7073fcab457751537c04
snoaman0 <snoaman@student.ethz.ch>
2023-10-16 14:00:18 +0200

    meeting 16.10

--- warmup_sara.py
+++ warmup_sara.py
@@ -10,34 +10,51 @@ b = 2
 c = 0
 d = 2
 
+n = 100
+
+
 def f(): #red line
 
-    theta = np.linspace(0, np.pi, 100)
+    theta = np.linspace(np.pi, 0, n)
     x1 = r1 * np.cos(theta) 
     y1 = r1 * np.sin(theta) 
     x2 = r2 * np.cos(theta) + c2[0]
     y2 = r2 * np.sin(theta)
     fig, ax = plt.subplots()
+    x_finish = []
+    y_finish = []
+    
+    counter = 0
 
-    for x in x1:
-        if x < c1[0]:
-            -r1 == - 1.5
-            r2 == 0
-        if x > c2[0]:
-            r2 == 1.7
-            r1 == 0
-    else:
-        x1 == 0
-        x2 == 0
+    for x_large in x1:
+        counter += 1
+        for x_small in x2:
+            if round(x_large,2) == round(x_small,2):
+                print(counter)
+                yneu = y1[counter-1]
+                y2neu = y2[counter-1]
+                if round(yneu,1) == round(y2neu,1):
+                   print('kaka')
+                   break
 
-    ax.plot(x1, y1, x2, y2)
+    counter = 93
+    print('hai',counter)                     
+    for i in range(0,counter):
+        x_finish.append(x1[i])
+        y_finish.append(y1[i])
+
+    for j in range(counter+1,n-1):
+        x_finish.append(x2[j])
+        y_finish.append(y2[j])
+
+    ax.plot(x_finish, y_finish)
     ax.set_xlim(a, b)
     ax.set_ylim(c, d)
     ax.set_aspect('equal') #aspect ratio to ensure the half-circle appears as a half-circle
     ax.set_xlabel('X-axis')
     ax.set_ylabel('Y-axis')
     plt.show()
-    plt.rcParams['figure.max_open_warning'] = 20
+    #plt.rcParams['figure.max_open_warning'] = 20
     return
 print(f())
 
